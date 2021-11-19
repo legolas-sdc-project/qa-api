d5e8344a5d098ce25901be4ed3198fab34f2cdf6
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-19 02:01:07 -0800

    created a database folder with index.js for postgres database connction.

---
+++ database/index.js
@@ -0,0 +1,28 @@
+const {Sequelize, Model, DataTypes} = require('sequelize');
+
+const db = new Sequelize('postgresql://chiaoling:@localhost:5432/sdc');
+
+const Questions = db.define("questions", {
+  id: DataTypes.INTEGER
+});
+
+module.exports = {
+  findOne: (req, res) => {
+    const id = req.params.id;
+    Questions.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Question with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Question with id=" + id
+      });
+    });
+  }
+}
